3c82eba1f985e196e91626eb73c24bbd4cecaba3
Abdelrahman Khater <132941382+Khater89@users.noreply.github.com>
2026-03-09 12:50:51 +0300

    Update folder_id to use only the ID string

--- drive_config.js
+++ drive_config.js
@@ -1,4 +1,4 @@
 const DRIVE_CONFIG = {
-  folder_id: "https://drive.google.com/drive/folders/1SI9HBx1LL_Hjgyz1jt-sKyqUFOo1kr4T?usp=drive_link",
+  folder_id: "1SI9HBx1LL_Hjgyz1jt-sKyqUFOo1kr4T?usp=drive_link",
   api_key:   "AIzaSyAHRQGKTgVlFb2TfWaObXDS_q3ZoaRVdhU",
 };
